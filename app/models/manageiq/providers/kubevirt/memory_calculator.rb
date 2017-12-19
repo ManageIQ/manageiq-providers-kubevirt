3dd300adb1a36218cb50238688724bb62a2e18fe
Juan Hernandez <juan.hernandez@redhat.com>
2017-12-19 20:10:13 +0100

    Adjust the memory calculator to Kubevirt 0.2.0
    In Kubevirt 0.2.0 the way that virtual machine memory is specified has
    changed. In previous versions it was specified like this:
    
      memory:
        unit: MB
        value: 64
    
    See the example here:
    
      https://github.com/kubevirt/kubevirt/blob/v0.1.0/cluster/vm.yaml#L34-L36
    
    Since version 0.2.0 it is specified like this:
    
      memory: 64M
    
    See the example here:
    
      https://github.com/kubevirt/kubevirt/blob/v0.2.0/cluster/vm.yaml#L10
    
    This means that the `MemoryCalculator` class needs to be changed
    accordingly to support the new format.
    
    Also the provision request and the parsing of the reported virtual
    machines were updated to support the updated memory format.
    
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- app/models/manageiq/providers/kubevirt/memory_calculator.rb
+++ app/models/manageiq/providers/kubevirt/memory_calculator.rb
@@ -14,47 +14,100 @@
 # limitations under the License.
 #
 
+require 'bigdecimal'
+
 #
 # This class contains functions that do memory calculations.
 #
 class ManageIQ::Providers::Kubevirt::MemoryCalculator
   #
-  # Converts a value from one unit to another unit
+  # Converts a value containing an optional unit to another unit. For example, if the value is
+  # `2 MiB` and the unit is `KiB` the result will be 2048.
   #
-  # @param value [Integer] The value to convert.
-  # @param from_unit [String] ('B') The name of the unit used by the value, for example `KB`.
-  # @param to_unit [String] ('B') The name of the unit to convert to, for example `GiB`.
-  # @return [Integer] The converted value, rounded down to the nearest integer.
+  # @param value [String] The value to convert, with an optional unit suffix.
+  # @param to [Symbol] (:b) The name of the unit to convert to, for example `:gib`.
+  # @return [BigDecimal] The converted value.
   #
-  def self.convert(value, from_unit, to_unit)
-    from_unit ||= 'B'
-    from_multiplier = MEMORY_UNIT_MULTIPLIERS[from_unit]
-    to_unit ||= 'B'
-    to_multiplier = MEMORY_UNIT_MULTIPLIERS[to_unit]
-    value * from_multiplier / to_multiplier
+  def self.convert(value, to)
+    # Return nil if no value is given:
+    return nil unless value
+
+    # Try to extract the numeric value and the unit:
+    match = VALUE_RE.match(value)
+    raise ArgumentError, "The value '#{value}' isn't a valid memory unit" unless match
+    value = match[:value]
+    from = match[:suffix]
+
+    # Convert the value from string to big decimal to make sure that we don't loose precission:
+    value = BigDecimal.new(value)
+
+    # Do the conversion:
+    from_factor = scale_factor(from)
+    to_factor = scale_factor(to)
+    value * from_factor / to_factor
   end
 
   private
 
-  MEMORY_UNIT_MULTIPLIERS = {
-    'B' => 1,
+  #
+  # Regular expression used to match values and to separate them into the numeric value and the
+  # optional unit suffix.
+  #
+  VALUE_RE = /^\s*(?<value>[[:digit:]]+)\s*(?<suffix>[[:alpha:]]+)?\s*$/i
 
-    'KB' => 10**3,
-    'MB' => 10**6,
-    'GB' => 10**9,
-    'TB' => 10**12,
-    'PB' => 10**15,
-    'EB' => 10**18,
-    'ZB' => 10**21,
-    'YB' => 10**24,
+  #
+  # Scale factors associated to the diffeent unit suffixes.
+  #
+  SCALE_FACTORS = {
+    b: 1,
 
-    'KiB' => 2**10,
-    'MiB' => 2**20,
-    'GiB' => 2**30,
-    'TiB' => 2**40,
-    'PiB' => 2**50,
-    'EiB' => 2**60,
-    'ZiB' => 2**70,
-    'YiB' => 2**80
+    k: 10**3,
+    m: 10**6,
+    g: 10**9,
+    t: 10**12,
+    p: 10**15,
+    e: 10**18,
+    z: 10**21,
+    y: 10**24,
+
+    kb: 10**3,
+    mb: 10**6,
+    gb: 10**9,
+    tb: 10**12,
+    pb: 10**15,
+    eb: 10**18,
+    zb: 10**21,
+    yb: 10**24,
+
+    ki: 2**10,
+    mi: 2**20,
+    gi: 2**30,
+    ti: 2**40,
+    pi: 2**50,
+    ei: 2**60,
+    zi: 2**70,
+    yi: 2**80,
+
+    kib: 2**10,
+    mib: 2**20,
+    gib: 2**30,
+    tib: 2**40,
+    pib: 2**50,
+    eib: 2**60,
+    zib: 2**70,
+    yib: 2**80
   }.freeze
+
+  #
+  # Finds the scale factor that matches the given unit suffix.
+  #
+  # @param sufix [Symbol, String] The unit suffix, as symbol or string. For example `MiB` or `:mib`.
+  # @return [Integer] The scale factor corresponding to that unit.
+  #
+  def self.scale_factor(suffix)
+    suffix = suffix.downcase.to_sym if suffix.is_a?(String)
+    factor = SCALE_FACTORS[suffix]
+    raise ArgumentErr, "The value '#{suffix}' isn't a valid unit suffix" unless factor
+    factor
+  end
 end
