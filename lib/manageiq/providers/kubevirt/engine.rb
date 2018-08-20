2c255dbc37500dc3885c0632304801532289ece5
Milan Zazrivec <mzazrivec@redhat.com>
2018-08-20 15:42:15 +0200

    Add plugin display name

--- lib/manageiq/providers/kubevirt/engine.rb
+++ lib/manageiq/providers/kubevirt/engine.rb
@@ -3,6 +3,10 @@ module ManageIQ
     module Kubevirt
       class Engine < ::Rails::Engine
         isolate_namespace ManageIQ::Providers::Kubevirt
+
+        def self.plugin_name
+          _('KubeVirt Provider')
+        end
       end
     end
   end
