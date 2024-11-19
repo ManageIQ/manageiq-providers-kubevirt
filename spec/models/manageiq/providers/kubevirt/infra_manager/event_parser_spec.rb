f3a6c3757b1337f0fc838c0d2cce01c6f8272ded
Nasar Khan <nasarak98@gmail.com>
2024-11-19 16:44:59 -0500

    add virtualmachineinstance event parser specs

--- spec/models/manageiq/providers/kubevirt/infra_manager/event_parser_spec.rb
+++ spec/models/manageiq/providers/kubevirt/infra_manager/event_parser_spec.rb
@@ -18,6 +18,25 @@ describe ManageIQ::Providers::Kubevirt::InfraManager::EventParser do
     }
   end
 
+  let(:vmi_shutdown) do
+    {
+      "object" => {
+        "involvedObject" => {
+          "kind"      => "VirtualMachineInstance",
+          "namespace" => "test-namespace",
+          "name"      => "test-vm",
+          "uid"       => "123-456-789"
+        },
+        "metadata"       => {
+          "uid" => "987-654-321"
+        },
+        "reason"         => "ShuttingDown",
+        "message"        => "VirtualMachineInstance shuttingdown",
+        "lastTimestamp"  => "2024-11-19T16:29:11Z"
+      }
+    }
+  end
+
   context 'event_to_hash' do
     it 'parses vm started into event' do
       event = RecursiveOpenStruct.new(vm_started, :recurse_over_arrays => true)
@@ -28,6 +47,24 @@ describe ManageIQ::Providers::Kubevirt::InfraManager::EventParser do
         :source              => 'KUBEVIRT',
         :timestamp           => "2024-11-19T16:29:11Z",
         :message             => "VirtualMachine started",
+        :container_namespace => "test-namespace",
+        :full_data           => event.to_h,
+        :ems_id              => nil,
+        :ems_ref             => "987-654-321",
+        :vm_name             => "test-vm",
+        :vm_ems_ref          => "123-456-789"
+      )
+    end
+
+    it 'parses vmi shutdown into event' do
+      event = RecursiveOpenStruct.new(vmi_shutdown, :recurse_over_arrays => true)
+      hash = described_class.event_to_hash(event, nil, "KUBEVIRT")
+
+      expect(hash).to include(
+        :event_type          => 'VIRTUALMACHINEINSTANCE_SHUTTINGDOWN',
+        :source              => 'KUBEVIRT',
+        :timestamp           => "2024-11-19T16:29:11Z",
+        :message             => "VirtualMachineInstance shuttingdown",
         :container_namespace => "test-namespace",
         :full_data           => event.to_h,
         :ems_id              => nil,
