9626b5fd9a11cab019b0a3526a5f4304916a23e2
Adam Grare <adam@grare.com>
2024-12-11 14:23:58 -0500

    Replace fog-kubevirt with kubeclient

--- app/models/manageiq/providers/kubevirt/inventory/collector.rb
+++ app/models/manageiq/providers/kubevirt/inventory/collector.rb
@@ -27,27 +27,43 @@ class ManageIQ::Providers::Kubevirt::Inventory::Collector < ManageIQ::Providers:
     name = @target.name
     @nodes = {}
 
-    @manager.with_provider_connection(:namespace => @target.location) do |connection|
-      if @target.template?
-        @templates = [connection.template(name)]
-      else
-        @vms = [connection.vm(name)]
-        begin
-          @vm_instances = [connection.vm_instance(name)]
-        rescue
-          # target refresh of a vm might fail if it has no vm instance
-          _log.debug("The is no running vm resource for '#{name}'")
-        end
+    if @target.template?
+      @templates = [openshift_template_client.get_template(name, @target.location)]
+    else
+      @vms = [kubevirt_client.get_virtual_machine(name, @target.location)]
+      begin
+        @vm_instances = [kubevirt_client.get_virtual_machine_instance(name, @target.location)]
+      rescue Kubeclient::ResourceNotFoundError
+        # target refresh of a vm might fail if it has no vm instance
+        _log.debug("The is no running vm resource for '#{name}'")
       end
     end
   end
 
   def initialize_for_full_refresh
-    @manager.with_provider_connection do |connection|
-      @nodes = connection.nodes
-      @vms = connection.vms
-      @vm_instances = connection.vm_instances
-      @templates = connection.templates
+    @nodes        = kube_client.get_nodes
+    @vms          = kubevirt_client.get_virtual_machines
+    @vm_instances = kubevirt_client.get_virtual_machine_instances
+    @templates    = openshift_template_client.get_templates
+  end
+
+  def kube_client(api_group = nil)
+    api_path, api_version = api_group&.split("/")
+
+    options = {:service => "kubernetes"}
+    if api_path
+      options[:path] = "/apis/#{api_path}"
+      options[:version] = api_version
     end
+
+    @manager.parent_manager.connect(options)
+  end
+
+  def kubevirt_client
+    @kubevirt_client ||= kube_client("kubevirt.io/v1")
+  end
+
+  def openshift_template_client
+    @openshift_template_client ||= kube_client("template.openshift.io/v1")
   end
 end
