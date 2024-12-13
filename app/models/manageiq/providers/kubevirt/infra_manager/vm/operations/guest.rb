cff2e1cfd2376733d63d1e5128484a0e75f11353
Dhamo1107 <dhamodharansathish4533@gmail.com>
2024-12-13 20:09:10 +0530

    Add soft reboot & restart request for VMs

--- app/models/manageiq/providers/kubevirt/infra_manager/vm/operations/guest.rb
+++ app/models/manageiq/providers/kubevirt/infra_manager/vm/operations/guest.rb
@@ -2,13 +2,11 @@ module ManageIQ::Providers::Kubevirt::InfraManager::Vm::Operations::Guest
 
   def raw_reboot_guest
     kubevirt = ext_management_system.parent_manager.connect(:service => "kubernetes", :path => "/apis/subresources.kubevirt.io", :version => "v1")
-    #method need to call on this kubevirt to restart the vm
-
+    kubevirt.rest_client["namespaces/#{location}/virtualmachineinstances/#{name}/softreboot"].put({}.to_json, { 'Content-Type' => 'application/json' }.merge(kubevirt.get_headers))
   end
 
   def raw_reset
     kubevirt = ext_management_system.parent_manager.connect(:service => "kubernetes", :path => "/apis/subresources.kubevirt.io", :version => "v1")
-    #method need to call on this kubevirt to soft reboot the vm
-
+    kubevirt.rest_client["namespaces/#{location}/virtualmachines/#{name}/restart"].put({}.to_json, { 'Content-Type' => 'application/json' }.merge(kubevirt.get_headers))
   end
 end
