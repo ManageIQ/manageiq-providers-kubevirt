e904069bb2dcf6e4b3ab82fd242ac3e8773e5c6e
Adam Grare <adam@grare.com>
2025-02-18 16:28:36 -0500

    Use kubevirt for vm soft_reboot/restart

--- app/models/manageiq/providers/kubevirt/infra_manager/vm/operations/guest.rb
+++ app/models/manageiq/providers/kubevirt/infra_manager/vm/operations/guest.rb
@@ -1,12 +1,14 @@
 module ManageIQ::Providers::Kubevirt::InfraManager::Vm::Operations::Guest
 
   def raw_reboot_guest
-    kubevirt = ext_management_system.parent_manager.connect(:service => "kubernetes", :path => "/apis/subresources.kubevirt.io", :version => "v1")
-    kubevirt.rest_client["namespaces/#{location}/virtualmachineinstances/#{name}/softreboot"].put({}.to_json, { 'Content-Type' => 'application/json' }.merge(kubevirt.get_headers))
+    with_provider_connection do |connection|
+      connection.v1_soft_reboot(name, location)
+    end
   end
 
   def raw_reset
-    kubevirt = ext_management_system.parent_manager.connect(:service => "kubernetes", :path => "/apis/subresources.kubevirt.io", :version => "v1")
-    kubevirt.rest_client["namespaces/#{location}/virtualmachines/#{name}/restart"].put({}.to_json, { 'Content-Type' => 'application/json' }.merge(kubevirt.get_headers))
+    with_provider_connection do |connection|
+      connection.v1_restart(name, location)
+    end
   end
 end
