0472f237aef990f94e8f3be1ccbb2c35d98ad0e5
Adam Grare <adam@grare.com>
2025-02-19 14:01:38 -0500

    Use create_namespaced_virtual_machine_snapshot

--- app/models/manageiq/providers/kubevirt/infra_manager/vm/operations/snapshot.rb
+++ app/models/manageiq/providers/kubevirt/infra_manager/vm/operations/snapshot.rb
@@ -1,19 +1,24 @@
 module ManageIQ::Providers::Kubevirt::InfraManager::Vm::Operations::Snapshot
   def raw_create_snapshot(snap_name, _desc = nil, _memory = false)
-    kubevirt = ext_management_system.parent_manager.connect(:service => "kubernetes", :path => "/apis/snapshot.kubevirt.io", :version => "v1alpha1")
-    kubevirt.create_virtual_machine_snapshot(
-      :metadata => {
-        :namespace => location,
-        :name      => snap_name
-      },
-      :spec => {
-        :source => {
-          :apiGroup => "kubevirt.io",
-          :kind     => "VirtualMachine",
-          :name     => name
-        }
-      }
-    )
+    with_provider_connection do |connection|
+      snapshot_spec = Kubevirt::V1beta1VirtualMachineSnapshot.new(
+        :api_version => "snapshot.kubevirt.io/v1beta1",
+        :kind        => "VirtualMachineSnapshot",
+        :metadata    => Kubevirt::K8sIoApimachineryPkgApisMetaV1ObjectMeta.new(
+          :name      => snap_name,
+          :namespace => location
+        ),
+        :spec        => Kubevirt::V1beta1VirtualMachineSnapshotSpec.new(
+          :source => Kubevirt::K8sIoApiCoreV1TypedLocalObjectReference.new(
+            :api_group => "kubevirt.io",
+            :kind      => "VirtualMachine",
+            :name      => name
+          )
+        )
+      )
+
+      connection.create_namespaced_virtual_machine_snapshot(location, snapshot_spec)
+    end
   end
 end
 
