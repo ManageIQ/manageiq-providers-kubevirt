5663f26aa83d0c6f3a9dc1e17381537ca3ee47b0
Adam Grare <adam@grare.com>
2025-02-19 13:33:12 -0500

    Use v1_start/v1_stop for VM power operations

--- app/models/manageiq/providers/kubevirt/infra_manager/vm/operations/power.rb
+++ app/models/manageiq/providers/kubevirt/infra_manager/vm/operations/power.rb
@@ -1,17 +1,14 @@
 module ManageIQ::Providers::Kubevirt::InfraManager::Vm::Operations::Power
   def raw_start
-    ext_management_system.with_provider_connection(:namespace => location) do |connection|
-      # Retrieve the details of the virtual machine:
-      vm = connection.vm(name)
-      vm.start
+    with_provider_connection do |connection|
+      start_options = Kubevirt::V1StartOptions.new
+      connection.v1_start(name, location, start_options)
     end
   end
 
   def raw_stop
-    ext_management_system.with_provider_connection(:namespace => location) do |connection|
-      # Retrieve the details of the virtual machine:
-      vm = connection.vm(name)
-      vm.stop
+    with_provider_connection do |connection|
+      connection.v1_stop(name, location)
     end
   end
 end
