58b165580611ab7a050d61a9d0b5890a9ce7d617
Adam Grare <adam@grare.com>
2025-03-11 14:45:18 -0400

    Check all hosts in EMS for max cpus/memory
    Since Kubernetes will schedule the VM on whatever node has enough
    cpu/memory the current host shouldn't limit the max cpu/memory.

--- spec/models/manageiq/providers/kubevirt/infra_manager/vm/reconfigure_spec.rb
+++ spec/models/manageiq/providers/kubevirt/infra_manager/vm/reconfigure_spec.rb
@@ -20,9 +20,39 @@ describe ManageIQ::Providers::Kubevirt::InfraManager::Vm::Reconfigure do
   end
 
   describe "#max_vcpus" do
+    let!(:host1) { FactoryBot.create(:host_kubevirt, :ext_management_system => ems, :hardware => FactoryBot.create(:hardware, :cpu2x2)) }
+    let!(:host2) { FactoryBot.create(:host_kubevirt, :ext_management_system => ems, :hardware => FactoryBot.create(:hardware, :cpu4x2)) }
+
+    it "returns the highest cpu_total_cores for all hosts in the EMS" do
+      expect(vm.max_vcpus).to eq(8)
+    end
+
+    context "with a host in another EMS" do
+      let(:ems2)   { FactoryBot.create(:ems_kubevirt) }
+      let!(:host2) { FactoryBot.create(:host_kubevirt, :ext_management_system => ems2, :hardware => FactoryBot.create(:hardware, :cpu4x2)) }
+
+      it "doesn't use host with more CPUs from another EMS" do
+        expect(vm.max_vcpus).to eq(4)
+      end
+    end
   end
 
   describe "#max_memory_mb" do
+    let!(:host1) { FactoryBot.create(:host_kubevirt, :ext_management_system => ems, :hardware => FactoryBot.create(:hardware, :memory_mb => 2_048)) }
+    let!(:host2) { FactoryBot.create(:host_kubevirt, :ext_management_system => ems, :hardware => FactoryBot.create(:hardware, :memory_mb => 4_096)) }
+
+    it "returns the highest memory_mb for all hosts in the EMS" do
+      expect(vm.max_memory_mb).to eq(4_096)
+    end
+
+    context "with a host in another EMS" do
+      let(:ems2)   { FactoryBot.create(:ems_kubevirt) }
+      let!(:host2) { FactoryBot.create(:host_kubevirt, :ext_management_system => ems2, :hardware => FactoryBot.create(:hardware, :memory_mb => 4_096)) }
+
+      it "doesn't use host with more memory from another EMS" do
+        expect(vm.max_memory_mb).to eq(2_048)
+      end
+    end
   end
 
   describe "#build_config_spec" do
