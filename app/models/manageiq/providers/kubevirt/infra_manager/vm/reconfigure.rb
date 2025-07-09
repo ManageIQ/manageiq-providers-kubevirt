8cdb2c927a7c8394154996d27e8060e3da821093
Adam Grare <adam@grare.com>
2025-07-09 14:23:32 -0400

    Add validate_config_spec checking for invalid options

--- app/models/manageiq/providers/kubevirt/infra_manager/vm/reconfigure.rb
+++ app/models/manageiq/providers/kubevirt/infra_manager/vm/reconfigure.rb
@@ -15,7 +15,21 @@ module ManageIQ::Providers::Kubevirt::InfraManager::Vm::Reconfigure
     ext_management_system.host_hardwares.pluck(:memory_mb).max
   end
 
+  def validate_config_spec(options)
+    if flavor
+      if %i[number_of_sockets cores_per_socket vm_memory].any? { |key| options.key?(key) }
+        raise MiqException::MiqVmError, _("Setting CPUs / Memory not supported for instance_type VMs")
+      end
+    else
+      if options.key?(:flavor_id)
+        raise MiqException::MiqVmError, _("Setting instance_type not supported for template VMs")
+      end
+    end
+  end
+
   def build_config_spec(options)
+    validate_config_spec(options)
+
     patches = []
     if options[:flavor_id]
       flavor = ext_management_system.flavors.find(options[:flavor_id])
