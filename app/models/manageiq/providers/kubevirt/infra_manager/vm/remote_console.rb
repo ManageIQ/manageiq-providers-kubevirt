0c146478fb83f1b2481bf6baf4a6e9d804cedc26
ishaaq-48 <ishaaq.sr@xaasio.com>
2026-08-04 20:49:27 +0530

    Updated URL.Build logic to provider

--- app/models/manageiq/providers/kubevirt/infra_manager/vm/remote_console.rb
+++ app/models/manageiq/providers/kubevirt/infra_manager/vm/remote_console.rb
@@ -4,8 +4,15 @@ module ManageIQ::Providers::Kubevirt::InfraManager::Vm::RemoteConsole
   end
 
   def validate_remote_console_acquire_ticket(protocol, _options = {})
-    raise(MiqException::RemoteConsoleNotSupportedError, "#{protocol} remote console requires the vm to be registered with a management system.") if ext_management_system.nil?
-    raise(MiqException::RemoteConsoleNotSupportedError, "#{protocol} remote console requires the vm to be running.") unless state == "on"
+    if ext_management_system.nil?
+      raise(MiqException::RemoteConsoleNotSupportedError,
+            "#{protocol} remote console requires the vm to be registered with a management system.")
+    end
+
+    unless state == "on"
+      raise(MiqException::RemoteConsoleNotSupportedError,
+            "#{protocol} remote console requires the vm to be running.")
+    end
   end
 
   def remote_console_acquire_ticket(userid, originating_server, protocol)
@@ -13,17 +20,22 @@ module ManageIQ::Providers::Kubevirt::InfraManager::Vm::RemoteConsole
   end
 
   def remote_console_acquire_ticket_queue(protocol, userid)
-    task_opts = {:action => "acquiring VM #{name} #{protocol.to_s.upcase} remote console ticket for user #{userid}", :userid => userid}
+    task_opts = {
+      :action => "acquiring VM #{name} #{protocol.to_s.upcase} remote console ticket for user #{userid}",
+      :userid => userid
+    }
+
     queue_opts = {
       :class_name  => self.class.name,
       :instance_id => id,
-      :method_name => 'remote_console_acquire_ticket',
+      :method_name => "remote_console_acquire_ticket",
       :queue_name  => ext_management_system.queue_name_for_ems_operations,
       :priority    => MiqQueue::HIGH_PRIORITY,
-      :role        => 'ems_operations',
+      :role        => "ems_operations",
       :zone        => my_zone,
       :args        => [userid, MiqServer.my_server.id, protocol]
     }
+
     MiqTask.generic_action_with_callback(task_opts, queue_opts)
   end
 
@@ -38,15 +50,40 @@ module ManageIQ::Providers::Kubevirt::InfraManager::Vm::RemoteConsole
       :user       => User.find_by(:userid => userid),
       :vm_id      => id,
       :ssl        => true,
-      :protocol   => 'kubevirt_vnc',
+      :protocol   => "kubevirt_vnc",
       :secret     => SecureRandom.hex,
-      :url_secret => SecureRandom.hex
+      :url_secret => SecureRandom.hex,
+      :url        => build_kubevirt_vnc_url(api_uri)
     }
 
-    SystemConsole.launch_proxy_if_not_local(console_args, originating_server, api_uri.host, api_uri.port)
+    SystemConsole.launch_proxy_if_not_local(
+      console_args,
+      originating_server,
+      api_uri.host,
+      api_uri.port
+    )
   end
 
   def remote_console_html5_acquire_ticket(userid, originating_server = nil)
     remote_console_vnc_acquire_ticket(userid, originating_server)
+  end
+
+  private
+
+  def build_kubevirt_vnc_url(api_uri)
+    URI::Generic.build(
+      :scheme => "wss",
+      :host   => api_uri.host,
+      :port   => api_uri.port,
+      :path   => kubevirt_vnc_path
+    ).to_s
+  end
+
+  def kubevirt_vnc_path
+    "/apis/subresources.kubevirt.io/v1/namespaces/#{kubevirt_namespace}/virtualmachineinstances/#{name}/vnc"
+  end
+
+  def kubevirt_namespace
+    "default"
   end
 end
